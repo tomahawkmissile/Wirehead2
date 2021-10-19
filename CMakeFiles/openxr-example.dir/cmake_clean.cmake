8cbc1c7887cb73cba3ca0f49e0b839b144b51194
elijah <tomahawkmissile2@gmail.com>
2021-10-18 19:21:42 -0700

    Initial commit.

---
+++ CMakeFiles/openxr-example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openxr-example.dir/main.c.o"
+  "openxr-example.pdb"
+  "openxr-example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/openxr-example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
